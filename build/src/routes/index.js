ad26aafa19e7247a3bcc60e2c43aa0c5fb11d721
blackgik <echendu.chinedum@gmail.com>
2022-03-24 09:05:41 +0100

    deleted the build file

--- build/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-const UserRouter_1 = __importDefault(require("./UserRouter"));
-module.exports = (router) => {
-    router.use((0, UserRouter_1.default)());
-    return router;
-};
